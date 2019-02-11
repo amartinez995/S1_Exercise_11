38db9530c2b5821d0d31a6d17781f234bcd039c0
Aiden Martinez <amartinez995@west-mec.org>
2019-02-11 15:22:55 -0700

    Day 1 - Final Commit - Ready for Review
    Added Javascript clock and all its code, it works perfectly. Project is complete and ready for review.

---
+++ tny_script.js
@@ -0,0 +1,33 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Aiden Martinez
+   Date:   02.05.2019
+
+*/
+
+// window.alert("Welcome to Tulsa ");
+runClock();
+setInterval("runClock()", 1000);
+
+function runClock() {
+    var currentDay = new Date();
+    var dateStr = currentDay.toLocaleDateString();
+    var timeStr = currentDay.toLocaleTimeString();
+    document.getElementById("dateNow").innerHTML = dateStr + "<br/>" + timeStr;
+    var newYear = new Date("January 1, 2018");
+    var nextYear = currentDay.getFullYear() + 1;
+    newYear.setFullYear(nextYear);
+    var daysLeft = (newYear - currentDay) / (1000 * 60 * 60 * 24);
+    var hrsLeft = (daysLeft - Math.floor(daysLeft)) * 24;
+    var minsLeft = (hrsLeft - Math.floor(hrsLeft)) * 60;
+    var secsLeft = (minsLeft - Math.floor(minsLeft)) * 60;
+    document.getElementById("days").textContent = Math.floor(daysLeft);
+    document.getElementById("hrs").textContent = Math.floor(hrsLeft);
+    document.getElementById("mins").textContent = Math.floor(minsLeft);
+    document.getElementById("secs").textContent = Math.floor(secsLeft);
+}
